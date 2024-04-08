f1fd7032967dd1c62e6824f35c18ee1b948d51fc
Pradeep Yadav <136789010+YADUVANSHIXD@users.noreply.github.com>
2024-04-08 08:18:21 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ cʀᴇᴀᴛᴇʀ : <a href='tg://user?id={OWNER_ID}'>YADUVANSHI</a>\n○ sᴏᴜʀcᴇ cᴏᴅᴇ : <a href='https://t.me/pvt_source_code'>Click here</a>\n○ cʜᴀɴɴᴇʟ : @hot_stall\n○ cʜᴀɴɴᴇʟ 𝟸 : @fun_stall</b>",
+            text = f"<b>○ cʀᴇᴀᴛᴇʀ : <a href='tg://user?id={OWNER_ID}'>YADUVANSHI</a>\n○ sᴏᴜʀcᴇ cᴏᴅᴇ : <a href='https://t.me/+CIXq0QHYnzdiMWM1'>Click here</a>\n○ cʜᴀɴɴᴇʟ : @hot_stall\n○ cʜᴀɴɴᴇʟ 𝟸 : @fun_stall</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
