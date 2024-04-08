c3b344b0ba83ac6ab005435722370615e35a1749
Pradeep Yadav <136789010+YADUVANSHIXD@users.noreply.github.com>
2024-04-08 07:43:02 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -8,7 +8,7 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, 
 @Bot.on_callback_query()
 async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
-    if data == "ᴀʙᴏᴜᴛ":
+    if data == "about":
         await query.message.edit_text(
             text = f"<b>○ cʀᴇᴀᴛᴇʀ : <a href='tg://user?id={OWNER_ID}'>YADUVANSHI</a>\n○ sᴏᴜʀcᴇ cᴏᴅᴇ : <a href='https://t.me/pvt_source_code'>Click here</a>\n○ cʜᴀɴɴᴇʟ : @hot_stall\n○ cʜᴀɴɴᴇʟ 𝟸 : @fun_stall</b>",
             disable_web_page_preview = True,
@@ -20,7 +20,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
                 ]
             )
         )
-    elif data == "CLOSE":
+    elif data == "close":
         await query.message.delete()
         try:
             await query.message.reply_to_message.delete()
